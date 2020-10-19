e3559992f08a6eb53aca082ffedfe8903eb3762e
hau1998 <54528392+hau1998@users.noreply.github.com>
2020-10-20 01:34:21 +0800

    Add files via upload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,42 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Event Management</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<!-- <link rel="stylesheet" href="{{asset('assets/css/main.css')}}" /> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<body background="/images/event.jpg">
+<div id="app">
+        
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" style="background-color: #d9d9d9;">
+            <div class="container" >
+                
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" >
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    <ul class="navbar-nav mr-auto" >
+                    <li class="nav-item"><a  class="nav-link" href="{{route('land')}}" style="font-size:15px;font-family:Helvetica; color:#b30000;"><b>Home</b></a></li>
+					<li class="nav-item"><a  class="nav-link" href="{{route('package')}}" style="font-size:15px;font-family:Helvetica; color:#b30000;"><b>Package</b></a></li>
+					<li class="nav-item"><a  class="nav-link" href="{{route('gallery')}}" style="font-size:15px;font-family:Helvetica; color:#b30000;"><b>Gallery</b></a></li>
+					<li class="nav-item"><a   class="nav-link" href="" style="font-size:15px;font-family:Helvetica; color:#b30000;"><b>Contact</b></a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -41,20 +44,18 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}" style="font-size:14px;font-family:Bradley Hand, cursive; color:#0000b3;"><i><u>{{ __('Login') }}</u></i></a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}" style="font-size:14px;font-family:Bradley Hand, cursive; color:#0000b3;"><i><u>{{ __('Register') }}</u></i></a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a style="font-size:14px;font-family:Bradley Hand, cursive; color:#0000b3;" id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="font-size:14px;">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -68,9 +69,17 @@
                             </li>
                         @endguest
                     </ul>
-                </div>
+                    </div>
+
+               
+                
             </div>
         </nav>
+
+      
+                    
+                
+               
 
         <main class="py-4">
             @yield('content')
