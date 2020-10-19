e43fe3973977af6682b62cb546e4ff14f02abb91
hau1998 <54528392+hau1998@users.noreply.github.com>
2020-10-20 01:37:42 +0800

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,28 @@
-@extends('layouts.default')
-
-@section('title', 'Home')
-
-@section('subheader', 'Home')
-@section('subheader-link', route('home'))
-
-@section('subheader-action', '')
-
-@section('pagevendorsstyles')
-@endsection
+@extends('layouts.app')
 
 @section('content')
+<div class="container">
+@if (session()->has('success'))
+            <h6 class="alert alert-success mt-5">{{ session('success') }}</h6>
+         @endif
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header" style="text-align:center;font-size:28px;font-family:Bradley Hand, cursive; color:#b30000;"><b>Dashboard</b></div>
+<br>
+                <div class="card-body" style="font-size: 18px;text-align:center;"><i>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+                    You are now logged in!
+                </i></div>
+				<br>
+				<br>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
