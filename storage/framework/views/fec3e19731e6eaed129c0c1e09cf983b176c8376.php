0060f8ac25096a2d9a46cb8ed8c81187928e9bcd
Yasshmin95 <58653218+Yasshmin95@users.noreply.github.com>
2020-11-28 15:14:58 +0800

    Add files via upload

---
+++ storage/framework/views/fec3e19731e6eaed129c0c1e09cf983b176c8376.php
@@ -0,0 +1,4 @@
+<div id="kt_scrolltop" class="kt-scrolltop">
+    <i class="fa fa-arrow-up"></i>
+</div>
+<?php /**PATH C:\Users\USER\xampp\htdocs\system\resources\views/includes/scrolltop.blade.php ENDPATH**/ ?>
